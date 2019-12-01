1fd61896e151246c2b3d2e728429b1ad4a002993
Markus Unger <schaulustig23@gmail.com>
2019-12-01 20:18:15 +0100

    finished solution for day 2

--- day01/solution.js
+++ day01/solution.js
@@ -3,10 +3,30 @@ const fs = require('fs').promises;
 const input = fs.readFile('input.txt', 'utf8');
 
 const calcFuelMass = mass => Math.floor(mass / 3) - 2;
+const totalFuelMass = (mass) => {
+  const fuelMass = calcFuelMass(mass);
+  return fuelMass <= 0 ? 0 : fuelMass + totalFuelMass(fuelMass);
+};
 
 (function solutionPart1() {
   input
     .then((masses) => {
-      console.log(masses.split('\n').reduce((sum, mass) => sum + calcFuelMass(mass), 0));
+      console.log(
+        masses
+          .split('\n')
+          .reduce((sum, mass) => sum + calcFuelMass(mass), 0),
+      );
+    });
+}());
+
+(function solutionPart2() {
+  input
+    .then((masses) => {
+      console.log(
+        masses
+          .split('\n')
+          .map(totalFuelMass)
+          .reduce((sum, mass) => sum + mass),
+      );
     });
 }());
