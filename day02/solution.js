a54e5d65cacfb663b39dcc56dd1df78f7851aae9
Markus Unger <schaulustig23@gmail.com>
2019-12-02 20:29:36 +0100

    finished solution for day 2

--- day02/solution.js
+++ day02/solution.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 
-const intcode = fs
+const input = fs
   .readFileSync('input.txt', 'utf8')
   .split(',')
   .map(Number);
 
-(function solutionPart1() {
+function runCode(pr1, pr2) {
+  const intcode = input.slice();
   let ptr = 0;
-  intcode[1] = 12;
-  intcode[2] = 2;
+  intcode[1] = pr1;
+  intcode[2] = pr2;
   while (intcode[ptr] !== 99) {
     const v1 = intcode[intcode[ptr + 1]];
     const v2 = intcode[intcode[ptr + 2]];
@@ -17,5 +18,22 @@ const intcode = fs
     if (intcode[ptr] === 2) intcode[tg] = v1 * v2;
     ptr += 4;
   }
-  console.log(intcode[0]);
+  return intcode[0];
+}
+
+(function solutionPart1() {
+  const solution = runCode(12, 2);
+  console.log(solution);
+}());
+
+(function solutionPart2() {
+  for (let pr1 = 0; pr1 <= 99; pr1 += 1) {
+    for (let pr2 = 0; pr2 <= 99; pr2 += 1) {
+      const solution = runCode(pr1, pr2);
+      if (solution === 19690720) {
+        console.log(100 * pr1 + pr2);
+        process.exit();
+      }
+    }
+  }
 }());
