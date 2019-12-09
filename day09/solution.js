e8afe0f7370810c427059fcb9e5952069ded3fb3
Markus Unger <schaulustig23@gmail.com>
2019-12-09 14:23:43 +0100

    refactoring the instruction parser, still a bit buggy, but much better than before

--- day09/solution.js
+++ day09/solution.js
@@ -15,7 +15,10 @@
 
 const fs = require('fs');
 
-const file = fs.readFileSync('input.txt', 'utf8').split(',').map(Number);
+// const file = fs.readFileSync('input.txt', 'utf8').split(',').map(Number);
+const file = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+  1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+  999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99];
 
 const intcodeProcessor = {
   init: function init(input) {
@@ -39,95 +42,92 @@ const intcodeProcessor = {
   },
 
   parse: function parse(instruction) {
-    const ARITIES = {
-      1: 3,
-      2: 3,
-      3: 1,
-      4: 1,
-      5: 2,
-      6: 2,
-      7: 3,
-      8: 3,
-      9: 1,
-      99: 0,
-    };
+    const ARITIES = { 1: 3, 2: 3, 3: 1, 4: 1, 5: 2, 6: 2, 7: 3, 8: 3, 9: 1, 99: 0, };
 
     const instr = instruction.toString();
-    const params = instr.slice(0, -2).split('').reverse().map(Number);
-    // TODO: create addresses and values to return with object
+    const opcode = Number(instr.slice(-1));
+    const paramModes = instr.slice(0, -2).split('').reverse().map(Number);
+
+    const params = [...Array(ARITIES[opcode]).keys()].reduce((info, num) => {
+      const v1 = this.intcode[this.ptr + num + 1];
+      const paramMode = paramModes[num] ? paramModes[num] : 0;
+      const address = this.intcode[this.ptr + num + 1];
+      if (paramMode === 2) address += this.relBase;
+      let value;
+      if (paramMode === 0 || paramMode === 2) value = this.intcode[this.intcode[address]];
+      if (paramMode === 1) value = this.intcode[address];
+      info.addresses.push(address);
+      info.values.push(value);
+      info.paramModes.push(paramMode);
+      return info;
+    }, { addresses: [], values: [], paramModes: [] });
+
     return {
       string: instr,
-      opcode: Number(instr.slice(-1)),
-      params,
+      opcode,
+      addresses: params.addresses,
+      values: params.values,
+      paramModes: params.paramModes,
     };
   },
 
   execute: function execute(instr) {
+    console.log(instr);
     const ic = this.intcode;
+    const { addresses, values } = instr;
     const { ptr } = this;
     switch (instr.opcode) {
       case 1: {
-        const a = instr.params[0] === 0 ? ic[ic[ptr + 1]] : ic[ptr + 1];
-        const b = instr.params[1] === 0 ? ic[ic[ptr + 2]] : ic[ptr + 2];
-        ic[ic[ptr + 3]] = a + b;
+        ic[addresses[2]] = values[0] + values[1];
         return 4;
       }
 
       case 2: {
-        const a = instr.params[0] === 0 ? ic[ic[ptr + 1]] : ic[ptr + 1];
-        const b = instr.params[1] === 0 ? ic[ic[ptr + 2]] : ic[ptr + 2];
-        ic[ic[ptr + 3]] = a * b;
+        ic[addresses[2]] = values[0] * values[1];
         return 4;
       }
 
       case 3: {
-        ic[ic[ptr + 1]] = this.input;
+        ic[addresses[0]] = this.input;
         return 2;
       }
 
       case 4: {
-        console.log(instr.params[0] === 0 ? ic[ic[ptr + 1]] : ic[ptr + 1]);
+        console.log(values[0]);
         return 2;
       }
 
       case 5: {
-        const a = instr.params[0] === 0 ? ic[ic[ptr + 1]] : ic[ptr + 1];
-        const b = instr.params[1] === 0 ? ic[ic[ptr + 2]] : ic[ptr + 2];
-        if (a !== 0) {
-          this.ptr = b;
+        if (values[0] !== 0) {
+          this.ptr = values[1];
           return 0;
         }
         return 3;
       }
 
       case 6: {
-        const a = instr.params[0] === 0 ? ic[ic[ptr + 1]] : ic[ptr + 1];
-        const b = instr.params[1] === 0 ? ic[ic[ptr + 2]] : ic[ptr + 2];
-        if (a === 0) {
-          this.ptr = b;
+        if (values[0] === 0) {
+          this.ptr = values[1];
           return 0;
         }
         return 3;
       }
 
       case 7: {
-        const a = instr.params[0] === 0 ? ic[ic[ptr + 1]] : ic[ptr + 1];
-        const b = instr.params[1] === 0 ? ic[ic[ptr + 2]] : ic[ptr + 2];
-        if (a < b) ic[ic[ptr + 3]] = 1;
-        else ic[ic[ptr + 3]] = 0;
+        if (values[0] < values[1]) ic[addresses[2]] = 1;
+        else ic[addresses[2]] = 0;
         return 4;
       }
 
       case 8: {
-        const a = instr.params[0] === 0 ? ic[ic[ptr + 1]] : ic[ptr + 1];
-        const b = instr.params[1] === 0 ? ic[ic[ptr + 2]] : ic[ptr + 2];
-        if (a === b) ic[ic[ptr + 3]] = 1;
-        else ic[ic[ptr + 3]] = 0;
+        if (values[0] === values[1]) ic[addresses[2]] = 1;
+        else ic[addresses[2]] = 0;
         return 4;
       }
 
       case 9: {
-        break;
+        this.relBase += values[0];
+        return 2;
       }
 
       default:
@@ -147,4 +147,4 @@ const intcodeProcessor = {
   },
 };
 
-intcodeProcessor.init(1).run();
+intcodeProcessor.init(8).run();
